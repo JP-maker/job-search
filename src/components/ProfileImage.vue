d4a706e868c90714a8251d82a742c4c69d456540
Jean-philippe Carraze <jpcarraze@ik.me>
2022-06-06 16:32:23 +0200

    Add button with differents styles

---
+++ src/components/ProfileImage.vue
@@ -0,0 +1,17 @@
+<template>
+  <img :src="imageLink" class="w-8 h-8 object-contain rounded-3xl" />
+</template>
+
+<script>
+export default {
+  name: "ProfileImage",
+  data() {
+    return {
+      imageLink:
+        "https://i.picsum.photos/id/866/200/300.jpg?hmac=rcadCENKh4rD6MAp6V_ma-AyWv641M4iiOpe1RyFHeI",
+    };
+  },
+};
+</script>
+
+<style></style>
